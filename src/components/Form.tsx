9a7a399ab40dd5f95bd9342d140c519e0ab35873
sajadsoqrati <sajadsoqrati@gmail.com>
2025-07-27 19:52:27 +0330

    add .env.local file and change input width

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -43,26 +43,26 @@ const Form = () => {
       onSubmit={handleSubmit(onSubmit)}
       className={
         isSmallScreen
-          ? "rounded-xl py-4 pb-4  bg-[rgba(60, 60, 60, 1)] w-93 "
-          : "rounded-xl py-4 pb-4 px-8 bg-[rgb(58,58,58)] w-109 "
+          ? "rounded-xl py-4 pb-4  bg-[rgba(60, 60, 60, 1)] w-85 "
+          : "rounded-xl py-4 pb-4 pl-8 pr-4 bg-[rgb(58,58,58)] w-109 "
       }
     >
       <div className="flex flex-col items-start">
-        <label htmlFor="Name" className=" text-2xl mb-1 text-white ">
+        <label htmlFor="Name" className=" text-2xl mb-1 ml-1 text-white ">
           Name*
         </label>
         <div>
           <input
-            placeholder="First"
+            placeholder="Firstname"
             {...register("firstName" /*{ required: true, minLength: 3 }*/)}
             type="text"
-            className=" bg-white text-black rounded-sm  mr-2"
+            className=" bg-white text-black rounded-sm w-40 ml-1 mr-1"
           />
           <input
-            placeholder="Last"
+            placeholder="Lastname"
             {...register("lastName" /*{ required: true, minLength: 3 }*/)}
             type="text"
-            className=" bg-white text-black rounded-sm ml-2"
+            className=" bg-white text-black rounded-sm ml-1 w-40"
           />
         </div>
         {errors.firstName /*?.type === "required"*/ && (
@@ -76,44 +76,53 @@ const Form = () => {
 
         <label
           htmlFor="emailAddress"
-          className=" text-2xl mb-1  mt-6 text-white "
+          className=" text-2xl mb-1 ml-1 mt-6 text-white "
         >
           Email Address*
         </label>
         <input
           {...register("email", { required: true })}
           type="email"
-          className="bg-white text-black pr-48 rounded-sm"
+          className="bg-white text-black pr-48 w-82 ml-1 rounded-sm"
         />
         {errors.email && <p className="text-red-500">{errors.email.message}</p>}
         {/* {errors.email?.type === "required" && (
           <p>The Email Field Is Required</p>
         )}  */}
-        <label htmlFor="phone" className=" text-2xl mb-1.5  mt-8 text-white ">
+        <label
+          htmlFor="phone"
+          className=" text-2xl mb-1.5 ml-1 mt-8 text-white "
+        >
           Phone*
         </label>
         <input
           {...register("tel", { valueAsNumber: true })}
           type="tel"
-          className="bg-white text-black pr-48 rounded-sm"
+          className="bg-white text-black pr-48 ml-1 w-82 rounded-sm"
         />
         {errors.tel && <p className="text-red-500">{errors.tel.message}</p>}
-        <label htmlFor="topic" className=" text-2xl mb-1.5  mt-8 text-white ">
+        <label
+          htmlFor="topic"
+          className=" text-2xl mb-1.5 ml-1 mt-8 text-white "
+        >
           Topic
         </label>
 
         <input
           {...register("topic")}
           type="text"
-          className="bg-white text-black pr-48 rounded-sm"
+          className="bg-white text-black pr-48 ml-1 w-82 rounded-sm"
         />
-        <label htmlFor="help" className=" text-2xl mb-1.5  mt-8 text-white ">
+        <label
+          htmlFor="help"
+          className=" text-2xl mb-1.5 ml-1  mt-8 text-white "
+        >
           How Can We Help You?*
         </label>
         <textarea
           {...register("help")}
           name="help"
-          className="bg-white text-black  h-20  w-93 text-wrap rounded-sm"
+          className="bg-white text-black  h-20  w-82 text-wrap rounded-sm"
         ></textarea>
       </div>
       <div className=" flex justify-center">
